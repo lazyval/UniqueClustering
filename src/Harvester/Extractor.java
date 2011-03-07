0a99dfd0c3b9e32322e219f252fe3ce3047134f3
John <john@ubuntu.(none)>
2011-03-07 04:29:05 -0500

    First java code file

---
+++ src/Harvester/Extractor.java
@@ -0,0 +1,11 @@
+package Harvester;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: john
+ * Date: 07.03.11
+ * Time: 4:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class Extractor {
+}
