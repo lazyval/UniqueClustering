6e4ed91bf891d430bfeaadf82f2eb0e0255848c0
JohnySilver7 <john@ubuntu.(none)>
2011-03-11 19:46:28 +0300

    Fixing missed commas in script

--- sql_scripts/create_table.sql
+++ sql_scripts/create_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cars (
          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
          price INT,
-         description VARCHAR(1024)
-         city VARCHAR(30)
+         description VARCHAR(1024),
+         city VARCHAR(30),
          product_year INT
        );
